71f31560e28e2efc039ebd24eb7d212d52fbc11d
Rubens Fernando Maciel Alencar <rubens.alencar@grupofolha.com.br>
2019-12-09 20:25:50 -0300

    Adicionand o express

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect(
+  `mongodb://mongo:27017/thoth`,
+  {
+    useNewUrlParser: true,
+  },
+);
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(cors());
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('/status', (req, res) => {
+  res.send('Up and running!');
+});
+
+app.listen(3003, () => console.log('Listening port 3003'));
